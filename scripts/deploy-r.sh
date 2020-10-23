2eef18d57eaf5b2a457b23390f5d56e572dadcfd
Ashish K Sahoo <ashissah@in.ibm.com>
2020-10-24 01:58:12 +0530

    Fix R
    Signed-off-by: Ashish K Sahoo <ashissah@in.ibm.com>

--- scripts/deploy-r.sh
+++ scripts/deploy-r.sh
@@ -14,7 +14,6 @@
 echo "Docker Push Images"
 docker push "$DOCKER_USERNAME"/$git_repo:$TRAVIS_BRANCH-$DEPLOY_TIMESTAMP-$TRAVIS_BUILD_NUMBER
 docker push "$DOCKER_USERNAME"/$git_repo:latest
-docker push "$DOCKER_USERNAME"/$git_repo:bk
 # ############################################################################
 # # Log into the IBM Cloud environment using apikey                          #
 # ############################################################################
